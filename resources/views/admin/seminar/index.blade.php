9cd282ad00e5723faedc3943340fe869f3757edf
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-10 23:00:40 +0800

    feat : add notification

--- resources/views/admin/seminar/index.blade.php
+++ resources/views/admin/seminar/index.blade.php
@@ -6,6 +6,17 @@
     </x-slot>
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg p-10">
+        @if (session('success'))
+            <div class="mb-4 text-green-700 bg-green-100 border border-green-400 p-4 rounded-lg">
+                {{ session('success') }}
+            </div>
+        @elseif (session('error'))
+            <div class="mb-4 text-red-700 bg-red-100 border border-red-400 p-4 rounded-lg">
+                {{ session('error') }}
+            </div>
+        @endif
+
+
         <a href="{{ route('admin.seminar.create') }}">
             <button type="button"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tambah
