9c0fd812f32144450a93b7b9ce0b808c9d64409b
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-10 17:54:49 +0800

    feat : add teamplate tabel seminar

--- resources/views/admin/seminar/index.blade.php
+++ resources/views/admin/seminar/index.blade.php
@@ -6,4 +6,45 @@
     </x-slot>
 
 
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg p-10">
+        <a href="{{ route('admin.kategori.create') }}">
+            <button type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tambah
+                Seminar</button>
+        </a>
+
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Image
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Nama seminar
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Kategori
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Narasumber
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Deskripsi
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Jumlah Kursi
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                    </th>
+
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+
 </x-app-layout>
