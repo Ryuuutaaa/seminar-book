6ed3221c6c7af6860f6fdf5b96d8f7755c45dea8
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-12 18:03:42 +0800

    adjust : card in dashboard peserta

--- resources/views/peserta/index.blade.php
+++ resources/views/peserta/index.blade.php
@@ -6,19 +6,19 @@
     </x-slot>
 
     <div class="py-12">
-        <section class="py-10">
+        <section>
             <div class="container mx-auto px-4">
                 <h2 class="text-2xl font-bold mb-6 text-white">Upcoming Seminars</h2>
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     @forelse($seminars as $seminar)
                         <div
-                            class="bg-white shadow-md rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+                            class="bg-gray-800 shadow-md rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                             <img src="{{ Storage::url($seminar->image) }}" alt="{{ $seminar->nama_seminar }}"
                                 class="w-full h-75 object-cover">
                             <div class="p-4">
-                                <h3 class="text-lg font-semibold mb-2">{{ $seminar->nama_seminar }}</h3>
-                                <p class="text-sm text-gray-600 mb-2">By: {{ $seminar->narasumber }}</p>
-                                <p class="text-sm text-gray-600 mb-2"><strong>Date:</strong>
+                                <h3 class="text-lg font-bold mb-2 text-white">{{ $seminar->nama_seminar }}</h3>
+                                <p class="text-sm text-white mb-2">By: {{ $seminar->narasumber }}</p>
+                                <p class="text-sm text-white mb-2"><strong>Date:</strong>
                                     {{ \Carbon\Carbon::parse($seminar->tanggal)->format('d M Y') }}
                                 </p>
                                 {{-- <a href="{{ route('seminar.detail', $seminar->id) }}"
