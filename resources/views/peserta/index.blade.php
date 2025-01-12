8f20cb073c64e8b5eff6e2af77486399773fd49d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-12 18:07:24 +0800

    feat : add button booking in dashboard peserta

--- resources/views/peserta/index.blade.php
+++ resources/views/peserta/index.blade.php
@@ -12,7 +12,7 @@
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     @forelse($seminars as $seminar)
                         <div
-                            class="bg-gray-800 shadow-md rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+                            class="bg-gray-800 shadow-md rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:cursor-pointer">
                             <img src="{{ Storage::url($seminar->image) }}" alt="{{ $seminar->nama_seminar }}"
                                 class="w-full h-75 object-cover">
                             <div class="p-4">
@@ -21,10 +21,8 @@
                                 <p class="text-sm text-white mb-2"><strong>Date:</strong>
                                     {{ \Carbon\Carbon::parse($seminar->tanggal)->format('d M Y') }}
                                 </p>
-                                {{-- <a href="{{ route('seminar.detail', $seminar->id) }}"
-                                    class="text-blue-500 hover:text-blue-700 text-sm font-semibold">
-                                    View Details
-                                </a> --}}
+                                <button type="button"
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 my-10">Booking</button>
                             </div>
                         </div>
                     @empty
