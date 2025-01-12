60ebef1d1805db94e2b30d49b19702e908e54f2a
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-13 01:53:14 +0800

    feat : add page konfirmasi pesanan and add code in controller

--- app/Http/Controllers/KonfrimasiPesananController.php
+++ app/Http/Controllers/KonfrimasiPesananController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class KonfrimasiPesananController extends Controller
 {
-    //
+    public function index()
+    {
+        return view("admin.kpesanan.index");
+    }
 }
