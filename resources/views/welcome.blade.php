435ed5f11b93128848ace556bdbe4fc1ae705c08
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-12 22:32:14 +0800

    feat : add route in joint seminar in welocme php to route login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
             <h2 class="text-2xl font-bold mb-6 text-gray-800">Upcoming Seminars</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
                 @forelse($seminars as $seminar)
-                    <div class="bg-white shadow-md rounded-lg overflow-hidden transition-all hover:scale-110 ">
+                    <div class="bg-white shadow-md rounded-lg overflow-hidden transition-all hover:scale-110  ">
                         <img src="{{ Storage::url($seminar->image) }}" alt="{{ $seminar->nama_seminar }}"
                             class="w-full h-75">
                         <div class="p-4">
@@ -48,7 +48,7 @@
                                 {{ \Carbon\Carbon::parse($seminar->tanggal_seminar)->format('d M Y') }}</p>
                             <p class="text-sm text-gray-600 mb-4"><strong>Seats:</strong> {{ $seminar->jumlah_kursi }}
                             </p>
-                            <a href="#"
+                            <a href="{{ route('login') }}"
                                 class="block bg-blue-500 text-white text-center py-2 rounded hover:bg-blue-600">Join
                                 Seminar</a>
                         </div>
