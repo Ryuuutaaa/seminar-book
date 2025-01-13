36ca359d134de8968c58718b8b067da8bb32e2be
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-13 16:14:56 +0800

    feat : add butto export PDF

--- resources/views/admin/seminar/index.blade.php
+++ resources/views/admin/seminar/index.blade.php
@@ -16,12 +16,20 @@
             </div>
         @endif
 
+        <div class="flex justify-between">
+            <a href="{{ route('admin.seminar.create') }}">
+                <button type="button"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tambah
+                    Seminar</button>
+            </a>
 
-        <a href="{{ route('admin.seminar.create') }}">
-            <button type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tambah
-                Seminar</button>
-        </a>
+            <a href="{{ route('admin.seminar.create') }}">
+                <button type="button"
+                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Export
+                    PDF</button>
+            </a>
+        </div>
+
 
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
